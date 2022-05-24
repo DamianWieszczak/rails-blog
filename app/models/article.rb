7fb5dc1c78cb98b148e345d891b0de7603600d2d
DamianWieszczak <damian.wieszczak@gmail.com>
2022-05-24 16:43:31 +0200

    Add validations to article.rb model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
-class Article < ApplicationRecord::Base
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 3, maximum: 60 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 400 }
 
-
-  
 end
