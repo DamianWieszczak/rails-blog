e7eb345634fa7d58cfb28a0573ba4b5f539a67c4
DamianWieszczak <damian.wieszczak@gmail.com>
2022-05-29 12:56:04 +0200

    Add comment feature and edit layout

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
 
   validates :title, presence: true, length: { minimum: 3, maximum: 60 }
   validates :description, presence: true, length: { minimum: 10, maximum: 400 }
